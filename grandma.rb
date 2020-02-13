e62bfee17d0b9c1ba53bdf0fd336f4dac3eabf45
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-13 21:12:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase !=phrase.upcase
     'HUH?! SPEAK UP, SONNY!'
-  elsif phrase=='ILOVE YOU GRANDMA!'
+  elsif phrase =='ILOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
   else 
     'NO, NOT SINCE 1938!'
